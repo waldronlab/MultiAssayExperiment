3c1f1fa58299c0ec79fae5e60140a30428ca9f4d
LiNk-NY <mramos09@gmail.com>
2015-08-11 17:30:23 -0400

    update
    Former-commit-id: 133a509c75e6af4f911db413528402fb262b7fb6

--- inst/extdata/tcga_ov/raw/rundates/tcgarundates.r
+++ inst/extdata/tcga_ov/raw/rundates/tcgarundates.r
@@ -1,11 +1,11 @@
 # TCGA Run dates script 
 # January 28, 2015
-# setwd("~/Documents/RTCGAToolbox/")
+library(RTCGAToolbox)
 # Function to format date times
-fdate <- function(dat){
-  dat <- gsub("([0-9]{4})([0-9]{2})", "\\1-\\2-", dat)
-  return(dat)
-}
+ fdate <- function(dat){
+   dat <- gsub("([0-9]{4})([0-9]{2})", "\\1-\\2-", dat)
+   return(dat)
+ }
 
 # all.dates <- rev(fdate(getFirehoseRunningDates()))
 # an.dates <- rev(fdate(getFirehoseAnalyzeDates()))
@@ -13,10 +13,10 @@ fdate <- function(dat){
 # dates <- data.frame(run = tail(all.dates, 5), analyze = tail(an.dates, 5), 
 #    checked = as.character(rep(Sys.Date(),5)), stringsAsFactors = FALSE)
 
-load("./rundates/rundates.rda")
+load("~/Documents/biocMultiAssay/inst/extdata/tcga_ov/raw/rundates/rundates.rda")
 if(dates$checked[nrow(dates)] != as.character(Sys.Date())) {
   dates <- rbind(dates, c(fdate(getFirehoseRunningDates(last = 1)), 
                  fdate(getFirehoseAnalyzeDates(last = 1)), as.character(Sys.Date())) )
-  save(dates, file = "rundates.rda")
+  save(dates, file = "~/Documents/biocMultiAssay/inst/extdata/tcga_ov/raw/rundates/rundates.rda")
 }
 
