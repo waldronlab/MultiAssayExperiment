39f03a383571cb99a00c0e73a14b6ac1e90a1cd1
LiNk-NY <mramos09@gmail.com>
2015-11-13 14:09:12 -0500

    minor change

--- R/toListMap.R
+++ R/toListMap.R
@@ -4,20 +4,21 @@
 #' @param dfmap A data frame object with identifiers in the first column
 #' @param assayCol A character vector of length one indicating the assay names column 
 #' @export
-toListMap <- function(dfmap, assayCol = NULL){
+toListMap <- function(mapdf, assayCol = NULL){
 	if(is.null(assayCol)){
 		stop("Provide the name of the column for assay names!")
 	}
-	if(!assayCol %in% colnames(dfmap)){
+	if(!assayCol %in% colnames(mapdf)){
 		stop("Assay names column not found in dataframe!")
 	}
 	if(!is.character(assayCol) | length(assayCol) != 1L){
 		stop("Assay name must be a string!")
 	}
-	colIndex <- which(colnames(dfmap) == assayCol)
-	newList <- split(dfmap[-colIndex], dfmap[,assayCol])
-	return(
-		   lapply(newList, FUN = function(x) { rownames(x) <- NULL
-				  x} )
-		   )
+	newList <- split(mapdf, mapdf[,assayCol])
+	newList <- lapply(newList, "[", c(1,2))
+	if(!is(dfmap, "DataFrame")){
+		newList <- lapply(newList, FUN = function(x) { rownames(x) <- NULL
+						  x} )
+	}
+	return(newList)
 }
