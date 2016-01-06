50dad95206e209394c28b8374e1eb0b3041d5295
Martin Morgan <martin.morgan@roswellpark.org>
2016-01-06 13:07:44 -0500

    update MultiAssayView - merge conflicts Conflicts: 	R/MultiAssayView-class.R

--- R/MultiAssayView-class.R
+++ R/MultiAssayView-class.R
@@ -1,73 +1,66 @@
 ### ==============================================
 ### MultiAssayView class
-### ----------------------------------------------
+### ==============================================
 
 #' An identifier class used for staging a subset operation
-#' 
+#'
 #' @slot query Any class indicator needed to subset
 #' @slot keeps A \code{list} indicating valid matches in each assay
 #' @slot drops A \code{list} of excluded information due to subsetting
 #' @slot type A \code{character} vector indicating method used to search
 #' @exportClass MultiAssayView
-setClass("MultiAssayView",
-         representation(query = "ANY",
-                        keeps = "list",
-                        drops = "list", 
-                        type = "character")
-)
 
-.checkDrops <- function(object) {
-  errors <- character()
-  if (length(object@keeps) != 0L) {
-    if (length(object@drops) != length(object@keeps)) {
-      msg <- paste0("List of dropped information must be the ",
-                    "same length as the kept information")
-      errors <- c(errors, msg)
-    }
-  }
-  if (length(errors) == 0L) 
-    NULL else errors
+.MultiAssayView <- setClass("MultiAssayView",
+         representation(
+             subject = "environment",
+             sampleMap="DataFrame",
+             features="list")
+         )
+
+MultiAssayView <- function(subject) {
+    .MultiAssayView(subject=as.environment(list(subject=subject)),
+                    sampleMap=sampleMap(subject))
 }
 
-.validMultiAssayView <- function(object) {
-  c(.checkDrops(object))
-}
+setMethod("sampleMap", "MultiAssayView", function(x) {
+    slot(x, "sampleMap")
+})
 
-S4Vectors::setValidity2("MultiAssayView", .validMultiAssayView)
+## FIXME
+setGeneric("sampleMap<-", function(x, value) standardGeneric("sampleMap<-"))
 
-#' Show method for \code{\linkS4class{MultiAssayView}} class
-#' 
-#' @param object A \code{\linkS4class{MultiAssayView}} class object
-#' @return A summary of \code{\linkS4class{MultiAssayView}} class contents 
-#' @exportMethod show
+setReplaceMethod("sampleMap", c("MultiAssayView", "DataFrame"),
+    function(x, value)
+{
+    slot(x, "sampleMap") <- value
+    x
+})
+
+setMethod("colnames", "MultiAssayView", function(x) {
+    map <- sampleMap(x)
+    split(map$assay, map$assayname)
+})
+
+setMethod("rownames", "MultiAssayView", function(x) {
+    ## FIXME
+})
+
+setMethod("[", c("MultiAssayView", "missing", "ANY"),
+    function(x, i, j, ..., drop=TRUE)
+{
+    ## FIXME: what are we subsetting on?
+    stopifnot(all(j %in% sampleMap(x)$master))
+    map <- sampleMap(x)
+    sampleMap(x) <- map[match(j, map$master),, drop=FALSE]
+    x
+})
+
 setMethod("show", "MultiAssayView", function(object) {
-  o_class <- class(object)
-  o_len <- length(object)
-  o_names <- names(object)
-  view_type <- type(object)
-  if (is.character(query(object))) {
-    o_ids <- Biobase::selectSome(query(object))
-  } else if (is(object, "GRanges")) {
-    o_ids <- names(object)
-  }
-  if (view_type != "assays") {
-    my_fun <- function(x) length(na.omit(x[, 1]))
-  } else {
-    my_fun <- function(x){
-      if (x) "keep"
-      else "drop"
-    }
-  }
-  v_ops <- sapply(object@keeps, FUN = my_fun)
-  cat("A", sprintf('"%s"', o_class), "class object of length",
-      paste0(o_len, ':'),
-      "\nQuery: ")
-  cat(o_ids, sep = ", ")
-  cat("\n Viewed by: ", '"', view_type, '"', sep = "")
-  viewed <- paste(v_ops, if(is.numeric(v_ops)){
-    ifelse(v_ops == 1L, gsub("s$", "", view_type), view_type)
-  }) 
-  cat(sprintf('\n [%i] %s: %s', seq(o_len), o_names,
-              viewed, 
-              "\n"))
+    map <- sampleMap(object)
+    assays <- unique(map$assayname)
+    cat("A ", class(object), " object",
+        "\nExperiments:\n  ",
+        paste(assays, collapse="\n  "),
+        "\nSamples: ", length(unique(map$master)),
+        "\n", sep="")
 })
