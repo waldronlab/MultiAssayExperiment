fd94bc8757a626d152f1ba06aa4c43eb00754572
Martin Morgan <martin.morgan@roswellpark.org>
2016-01-06 15:56:22 -0500

    use row/col integer index for subsetting

--- R/MultiAssayView-class.R
+++ R/MultiAssayView-class.R
@@ -2,65 +2,104 @@
 ### MultiAssayView class
 ### ==============================================
 
-#' An identifier class used for staging a subset operation
+#' An class used for staging a subset operation
 #'
-#' @slot query Any class indicator needed to subset
-#' @slot keeps A \code{list} indicating valid matches in each assay
-#' @slot drops A \code{list} of excluded information due to subsetting
-#' @slot type A \code{character} vector indicating method used to search
+#' @slot subject A MultiAssayExperiment instance
+#' @slot rowindex An \code{IntegerList} indexing rows of subject
+#'     retained for subsequent work.
+#' @slot colindex An \code{IntegerList} indexing columns of sujbect
+#'     retained for subsequent work.
 #' @exportClass MultiAssayView
 
 .MultiAssayView <- setClass("MultiAssayView",
          representation(
              subject = "environment",
-             sampleMap="DataFrame",
-             features="list")
+             rowindex="IntegerList",
+             colindex="IntegerList")
          )
 
-MultiAssayView <- function(subject) {
-    .MultiAssayView(subject=as.environment(list(subject=subject)),
-                    sampleMap=sampleMap(subject))
+.idxlist <- function(x) {
+    nms <- names(x)
+    x <- unname(CharacterList(x))
+    offset <- rep(cumsum(c(0L, lengths(x)[-length(x)])), lengths(x))
+    idx <- seq_along(unlist(x, use.names=FALSE)) - offset
+    setNames(relist(idx, x), nms)
 }
 
-setMethod("sampleMap", "MultiAssayView", function(x) {
-    slot(x, "sampleMap")
+MultiAssayView <- function(subject) {
+    stopifnot(inherits(subject, "MultiAssayExperiment"))
+    .MultiAssayView(subject=as.environment(list(subject=subject)),
+                    rowindex=.idxlist(rownames(subject)),
+                    colindex=.idxlist(colnames(subject)))
+}
+
+.subject <- function(x) {
+    getElement(x, "subject")[["subject"]]
+}
+
+.rowindex <- function(x)
+    getElement(x, "rowindex")
+
+.colindex <- function(x)
+    getElement(x, "colindex")
+
+setMethod("rownames", "MultiAssayView", function(x) {
+    CharacterList(rownames(.subject(x)))[.rowindex(x)]
 })
 
-## FIXME
-setGeneric("sampleMap<-", function(x, value) standardGeneric("sampleMap<-"))
-
-setReplaceMethod("sampleMap", c("MultiAssayView", "DataFrame"),
+setReplaceMethod("rownames", c("MultiAssayView", "ANY"),
     function(x, value)
 {
-    slot(x, "sampleMap") <- value
+    slot(x, "rownames") <-
+        match(CharacterList(value), rownames(x))
     x
 })
 
 setMethod("colnames", "MultiAssayView", function(x) {
-    map <- sampleMap(x)
-    split(map$assay, map$assayname)
+    CharacterList(colnames(.subject(x)))[.colindex(x)]
 })
 
-setMethod("rownames", "MultiAssayView", function(x) {
-    ## FIXME
-})
-
-setMethod("[", c("MultiAssayView", "missing", "ANY"),
-    function(x, i, j, ..., drop=TRUE)
+setReplaceMethod("colnames", c("MultiAssayView", "ANY"),
+   function(x, value)
 {
-    ## FIXME: what are we subsetting on?
-    stopifnot(all(j %in% sampleMap(x)$master))
-    map <- sampleMap(x)
-    sampleMap(x) <- map[match(j, map$master),, drop=FALSE]
+    slot(x, "colnames") <- CharacterList(x)
     x
 })
 
+.subset1 <- function(i, index, names)
+    ## FIXME: below is +/- ok for typeof(i) == character only
+    index[which(names %in% i)]
+
+setMethod("[", c("MultiAssayView", "ANY", "ANY"),
+    function(x, i, j, ..., drop=TRUE)
+{
+    rowindex <- .rowindex(x)
+    if (!missing(i))
+        rowindex <- .subset1(i, rowindex, rownames(x))
+    colindex <- .colindex(x)
+    if (!missing(j))
+        colindex <- .subset1(j, colindex, colnames(x))
+    initialize(x, rowindex=rowindex, colindex=colindex)
+    ## FIXME: row/colnames should be updated to reflect subsets ?
+})
+
+materialize <- function(x)
+{
+    stopifnot(inherits(x, "MultiAssayView"))
+
+    subject <- .subject(x)
+    elist <- Elist(subject)
+    elist <- mendoapply(function(e, i) e[i,,drop=FALSE], elist, rownames(x))
+    elist <- mendoapply(function(e, j) e[,j, drop=FALSE], elist, colnames(x))
+    initialize(subject, Elist=elist)
+}
+
 setMethod("show", "MultiAssayView", function(object) {
-    map <- sampleMap(object)
-    assays <- unique(map$assayname)
-    cat("A ", class(object), " object",
-        "\nExperiments:\n  ",
-        paste(assays, collapse="\n  "),
-        "\nSamples: ", length(unique(map$master)),
-        "\n", sep="")
+    rownames <- rownames(object)
+    colnames <- colnames(object)
+    cat("A", class(object), "object\n")
+    cat("\nrownames():\n")
+    print(rownames)
+    cat("\ncolnames():\n")
+    print(colnames)
 })
