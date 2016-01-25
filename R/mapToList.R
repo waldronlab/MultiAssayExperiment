1f16652875a418a5f40bc09eca653fc0cdc9e024
LiNk-NY <mramos09@gmail.com>
2016-01-25 13:39:48 -0500

    change argument name

--- R/mapToList.R
+++ R/mapToList.R
@@ -2,26 +2,26 @@
 #' 
 #' \code{mapToList} returns a \code{list} of DataFrames 
 #' 
-#' @param mapdf A \code{data.frame} or \code{DataFrame} object with
+#' @param object A \code{data.frame} or \code{DataFrame} object with
 #' identifiers in the first column
 #' @param assayCol A character vector of length one indicating the assay
 #' names column 
 #' @return A \code{list} object of DataFrames for each assay
 #' @export mapToList
-mapToList <- function(mapdf, assayCol = NULL) {
+mapToList <- function(object, assayCol = NULL) {
     if (is.null(assayCol)) {
         stop("Provide assaynames column reference")
     }
-    if (!assayCol %in% colnames(mapdf)) {
+    if (!assayCol %in% colnames(object)) {
         stop("assayname column not found in dataframe")
     }
     if (!is.character(assayCol) | length(assayCol) != 1L) {
         stop("assayname must be a string")
     }
-    if (is(mapdf, "data.frame")) {
-        mapdf <- S4Vectors::DataFrame(mapdf)
+    if (is(object, "data.frame")) {
+        object <- S4Vectors::DataFrame(object)
     }
-    newList <- S4Vectors::split(mapdf, mapdf[, assayCol])
+    newList <- S4Vectors::split(object, object[, assayCol])
     newList <- lapply(newList, "[", c(1, 2))
     return(newList)
 } 
