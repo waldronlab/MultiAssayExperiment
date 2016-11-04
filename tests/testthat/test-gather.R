71bc9def44bfbbbaf81a9b7eb0676b7f729b5682
LiNk-NY <mramos09@gmail.com>
2016-11-04 19:09:01 -0400

    add tests to gather

---
+++ tests/testthat/test-gather.R
@@ -0,0 +1,18 @@
+context("gather methods")
+
+example("MultiAssayExperiment")
+
+testMat <- matrix(seq_len(20), nrow = 4, ncol = 5, byrow = TRUE,
+                  dimnames = list(LETTERS[1:4], letters[1:5]))
+
+testESet <- Biobase::ExpressionSet(testMat)
+
+matDF <- gather(testMat)
+ESetDF <- gather(testESet)
+RRADF <- gather(myRRA)
+
+test_that("gather returns a DataFrame", {
+    expect_true(is(matDF, "DataFrame"))
+    expect_true(is(ESetDF, "DataFrame"))
+    expect_true(is(RRADF, "DataFrame"))
+})
