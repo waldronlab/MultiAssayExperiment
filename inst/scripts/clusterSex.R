600d460d372565d43e3dbe2f981422310b5cee19
LiNk-NY <mramos09@gmail.com>
2016-12-29 18:45:22 -0500

    formatting changes to clusterSex, close #173

--- inst/scripts/clusterSex.R
+++ inst/scripts/clusterSex.R
@@ -13,7 +13,7 @@
 clusterSex <- function(MultiAssayExperiment, pDataCols, ExperimentList.rows,
                       ExperimentList.exp, seed) {
     #Subset the MAE by rows(genes) and experiments
-    MAE_s <- MAE[ExperimentList.rows,, ExperimentList.exp]
+    MAE_s <- MAE[ExperimentList.rows, , ExperimentList.exp]
     #Use gather to get a dataframe with the metadata and experiment data
     meta_MAE <- gather(MAE_s,pDataCols = pDataCols)
 
@@ -24,27 +24,27 @@ clusterSex <- function(MultiAssayExperiment, pDataCols, ExperimentList.rows,
 
     #Normalization of values for each experimentList.
     subset<- meta_MAEw[,sapply(meta_MAEw, is.numeric)]
-    temp<- matrix(NA ,nrow = nrow(meta_MAEw),
+    temp<- matrix(NA, nrow = nrow(meta_MAEw),
                   ncol = length(ExperimentList.rows)) # temp as a matrix
     #browser()
     for (i in seq_along(subset)) {
-        temp[,i]<-scale(subset[,i])
+        temp[, i] <- scale(subset[, i])
     }
 
-    meta_MAEw_n<-as.data.frame(temp)
+    meta_MAEw_n <- as.data.frame(temp)
     colnames(meta_MAEw_n) <- ExperimentList.rows
     meta_MAEw_n$PatientID <- meta_MAEw$primary
     meta_MAEw_n$sampleID <- meta_MAEw$colname
     meta_MAEw_n$Mgender <- meta_MAEw$gender
 
     set.seed(seed)
-    kma <- kmeans(meta_MAEw_n[,ExperimentList.rows], centers =2)
+    kma <- kmeans(meta_MAEw_n[, ExperimentList.rows], centers =2)
     meta_MAEw_n$Cluster <- kma$cluster
     centers_a <- kma$centers
     Kcenters <- centers_a[meta_MAEw_n[["Cluster"]], ]
     colnames(Kcenters) <- ExperimentList.rows
 
-    genderscomp <- cbind(meta_MAEw_n[,c("PatientID", "Mgender","Cluster")],
+    genderscomp <- cbind(meta_MAEw_n[, c("PatientID", "Mgender","Cluster")],
                          Kcenters)
 
     return(genderscomp)
