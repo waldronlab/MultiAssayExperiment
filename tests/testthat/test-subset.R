c45495b81b986b3c57370b9c0099f99678f35e6b
LiNk-NY <mramos09@gmail.com>
2017-02-27 14:44:29 -0500

    add tests to subset methods

--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -20,20 +20,29 @@ test_that("subsettor length is of the same as MultiAssayExperiment", {
 })
 
 test_that("drop argument works", {
-  expect_equal(length(
-      experiments(myMultiAssayExperiment[, colList, drop = TRUE])), 3L)
-  expect_equal(length(
-      experiments(myMultiAssayExperiment[, colList, drop = FALSE])), 4L)
-  expect_equal(length(
-      experiments(myMultiAssayExperiment[rowList, drop = TRUE])), 3L)
-  expect_equal(length(
-      experiments(myMultiAssayExperiment[rowList, drop = FALSE])), 4L)
-  expect_equal(length(
-      experiments(myMultiAssayExperiment[FALSE, drop = TRUE])), 0L)
-  expect_equal(length(
-      experiments(myMultiAssayExperiment[FALSE, drop = FALSE])), 4L)
-  expect_equal(length(
-      experiments(myMultiAssayExperiment[, FALSE, drop = TRUE])), 0L)
-  expect_equal(length(
-      experiments(myMultiAssayExperiment[, FALSE, drop = FALSE])), 4L)
+  expect_equal(length(myMultiAssayExperiment[, colList, drop = TRUE]), 3L)
+  expect_equal(length(myMultiAssayExperiment[, colList, drop = FALSE]), 4L)
+  expect_equal(length(myMultiAssayExperiment[rowList, drop = TRUE]), 3L)
+  expect_equal(length(myMultiAssayExperiment[rowList, drop = FALSE]), 4L)
+  expect_equal(length(myMultiAssayExperiment[FALSE, drop = TRUE]), 0L)
+  expect_equal(length(myMultiAssayExperiment[FALSE, drop = FALSE]), 4L)
+  expect_equal(length(myMultiAssayExperiment[, FALSE, drop = TRUE]), 0L)
+  expect_equal(length(myMultiAssayExperiment[, FALSE, drop = FALSE]), 4L)
+})
+
+test_that("subsetByColumns works with lists", {
+    affySub <- list(Affy = 1:2)
+    affySimple <- List(affySub)
+    expect_equal(length(myMultiAssayExperiment[, affySub, ]), length(affySub))
+    expect_equal(length(myMultiAssayExperiment[, affySimple, ]),
+                 length(affySimple))
+})
+
+test_that("subsetBypData works as intended", {
+    trues <- sum(myMultiAssayExperiment$sex == "M")
+    expect_equal(nrow(pData(subsetBypData(myMultiAssayExperiment,
+                                          myMultiAssayExperiment$sex == "M"))),
+                 trues)
+    expect_equal(nrow(pData(
+        myMultiAssayExperiment[, myMultiAssayExperiment$sex == "M"])), trues)
 })
