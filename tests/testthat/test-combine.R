49adbed53f57a42ba067c7caf8e80d7f0372ab71
LiNk-NY <mramos09@gmail.com>
2017-03-20 20:44:51 -0400

    add tests

--- tests/testthat/test-combine.R
+++ tests/testthat/test-combine.R
@@ -1,3 +1,14 @@
+context("c function expands ExperimentList and modifies sampleMap")
+
+example("MultiAssayExperiment")
+
+## BiocInstaller::biocLite("Bioconductor/RaggedExperiment")
+
+library(SummarizedExperiment)
+library(RaggedExperiment)
+example("SummarizedExperiment")
+example("RaggedExperiment")
+
 test_that("combine c function works", {
     mat <- matrix(rnorm(20), ncol = 4, dimnames = list(NULL, LETTERS[1:4]))
     myMulti <- MultiAssayExperiment(list(express = mat))
@@ -8,3 +19,17 @@ test_that("combine c function works", {
     expect_true(is(c(myMulti, express2 = mat2, mapFrom = 1L),
                    "MultiAssayExperiment"))
 })
+
+test_that("combine c function works on multiple objects", {
+    rse1 <- rse1[, 1:4]
+    addMap <- DataFrame(assay = "RExp",
+                        primary = c("Jack", "Jill"),
+                        colname = c("sample1", "sample2"))
+    addMap <- rbind(addMap, DataFrame(assay = "myRSE",
+                                      primary = c("Jack", "Jill",
+                                                  "Bob", "Barbara"),
+                                      colname = LETTERS[1:4]))
+    expect_true(validObject(c(myMultiAssayExperiment, RExp = re3,
+                              myRSE = rse1, sampleMap = addMap)))
+})
+
