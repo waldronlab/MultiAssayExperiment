138049043acc42366c87de326439fc97f6ea12c8
LiNk-NY <mramos09@gmail.com>
2017-03-30 11:22:05 -0400

    format ExperimentList example

--- inst/scripts/ExperimentList-Ex.R
+++ inst/scripts/ExperimentList-Ex.R
@@ -7,17 +7,20 @@ arraydat <- matrix(seq(101, 108), ncol = 4,
                      c("ENST00000294241", "ENST00000355076"),
                      c("array1", "array2", "array3", "array4")
                    ))
+
 arraypdat <- as(data.frame(
-  slope53 = rnorm(4),
-  row.names = c("array1", "array2", "array3", "array4")),
-  "AnnotatedDataFrame")
+    slope53 = rnorm(4),
+    row.names = c("array1", "array2", "array3", "array4")),
+    "AnnotatedDataFrame")
+
+## ExpressionSet constructor
 exprdat <- Biobase::ExpressionSet(assayData = arraydat, phenoData = arraypdat)
 
 ## Create a sample methylation dataset
-methyldat <- matrix(1:10, ncol = 5,
-                    dimnames = list(
-                      c("ENST00000355076", "ENST00000383706"),
-                      c("methyl1", "methyl2", "methyl3", "methyl4", "methyl5")))
+methyldat <- matrix(1:10, ncol = 5, dimnames = list(
+    c("ENST00000355076", "ENST00000383706"),
+    c("methyl1", "methyl2", "methyl3",
+      "methyl4", "methyl5")))
 
 ## Create a sample RNASeqGene dataset
 rnadat <- structure(c(46851, 5, 19, 13, 2197, 507, 84318, 126, 17, 21,
