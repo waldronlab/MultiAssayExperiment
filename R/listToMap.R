9bf04ead47784ee3d34023e1d811d8ac5f871e98
LiNk-NY <mramos09@gmail.com>
2017-04-05 20:12:41 -0400

    minor update to listToMap function

--- R/listToMap.R
+++ R/listToMap.R
@@ -1,19 +1,21 @@
-#' @param listmap A \code{list} class object containing names of either 
-#' experiments, assays or features. 
-#' @param type Any of the valid types of maps including colnames, rownames, 
-#' and assays. 
+#' @param listmap A named \code{list} object containing either
+#' experiments (\code{assay}s), samples (\code{colname}s) or
+#' features (\code{rowname}s)
+#' @param type Any of the valid types of maps including colnames, rownames,
+#' and assays.
 #' @return A \code{DataFrame} class object of names
 #' @describeIn mapToList Inverse of the listToMap function
 #' @export listToMap
 listToMap <- function(listmap, type = "colnames") {
-    if (!type %in% c("colnames", "rownames", "assays"))
-        stop("Type not valid")
+    if (is.null(names(listmap)))
+        stop("'listmap' must be a named list")
+    type <- match.arg(type, c("colnames", "rownames", "assays"))
     DFmap <- lapply(seq_along(listmap), FUN = function(i, x) {
         if (type == "colnames") {
             if (S4Vectors::isEmpty(x[i])) {
                 S4Vectors::DataFrame(assay = factor(names(x)[i]),
-                                     primary = NA,
-                                     colname = NA)
+                                     primary = NA_character_,
+                                     colname = NA_character_)
             } else {
                 S4Vectors::DataFrame(assay = factor(names(x)[i]),
                                      primary = x[[i]][, 1],
@@ -22,7 +24,7 @@ listToMap <- function(listmap, type = "colnames") {
         } else if (type == "rownames") {
             if (S4Vectors::isEmpty(x[i])) {
                 S4Vectors::DataFrame(assay = factor(names(x)[i]),
-                                     rowname = NA)
+                                     rowname = NA_character_)
             } else {
                 S4Vectors::DataFrame(assay = factor(names(x)[i]),
                                      rowname = x[[i]])
