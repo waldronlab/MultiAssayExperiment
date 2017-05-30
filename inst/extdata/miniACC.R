204f3385e52143bbb070d0d549d0e5922f7f7a27
Marcel Ramos <mramos09@gmail.com>
2017-05-30 20:57:14 +0000

    update version 1.3.20
    - restore deleted unit test
    - clean up code
    - update vignettes
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MultiAssayExperiment@130009 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/extdata/miniACC.R
+++ inst/extdata/miniACC.R
@@ -5,34 +5,30 @@
 \dontrun{
     ## See www.tinyurl.com/MAEOurls for more pre-built TCGA MultiAssayExperiment objects
     download.file("http://s3.amazonaws.com/multiassayexperiments/accMAEO.rds",
-                  destfile = "~/Downloads/accMAEO.rds")
+                  destfile = "accMAEO.rds")
     library(MultiAssayExperiment)
     library(RaggedExperiment) #needed for RaggedExperiment objects by updateObject()
-    acc <- readRDS("~/Downloads/accMAEO.rds")
+    library(Biobase)
+
+    acc <- readRDS("accMAEO.rds")
     acc <- updateObject(acc)
-    protmap <-
-        read.csv(
-            "http://genomeportal.stanford.edu/pan-tcga/target_selection_send_data?filename=Allprotein.txt",
-            as.is = TRUE
+    protmap <- read.csv(paste0("http://genomeportal.stanford.edu/",
+                        "pan-tcga/target_selection_send_data",
+                        "?filename=Allprotein.txt"), as.is = TRUE
         )
 
-    RPPAgenes <- protmap$Genes
-    RPPAgenes <- c(RPPAgenes, unlist(strsplit(RPPAgenes, ",")))
+    RPPAgenes <- Filter(function(x) x != "", protmap$Genes)
+    RPPAgenes <- unlist(strsplit(RPPAgenes, ","))
     RPPAgenes <- unique(RPPAgenes)
-    RPPAgenes <- RPPAgenes[!RPPAgenes == ""]
 
     miniACC <-
         acc[RPPAgenes, , c("RNASeq2GeneNorm", "gistict", "RPPAArray", "Mutations")]
-    mut <- assays(miniACC[["Mutations"]])[["Variant_Classification"]]
-    mut[is.na(mut)] <- 0
-    mut[mut == "Silent"] <- 0
-    mut[mut != "0"] <- 1
-    class(mut) <- "numeric"
+    mut <- assay(miniACC[["Mutations"]], i = "Variant_Classification")
+    mut <- ifelse(is.na(mut) | mut == "Silent", 0, 1)
 
     miniACC[["Mutations"]] <- mut
     colData(miniACC) <- colData(miniACC)[, c(1:17, 810:822)]
 
-    library(Biobase)
     rpparowData <-
         protmap[match(rownames(miniACC[["RPPAArray"]]), protmap$Genes),]
     rpparowData <- AnnotatedDataFrame(rpparowData)
@@ -43,20 +39,20 @@
             title = "Comprehensive Pan-Genomic Characterization of Adrenocortical Carcinoma",
             PMID = "27165744",
             sourceURL = "http://s3.amazonaws.com/multiassayexperiments/accMAEO.rds",
-            RPPAfeatureDataURL = "http://genomeportal.stanford.edu/pan-tcga/show_target_selection_file?filename=Allprotein.txt",
+            RPPAfeatureDataURL = paste0("http://genomeportal.stanford.edu/",
+                                        "pan-tcga/show_target_selection_file",
+                                        "?filename=Allprotein.txt"),
             colDataExtrasURL = "http://www.cell.com/cms/attachment/2062093088/2063584534/mmc3.xlsx"
         )
     metadata(miniACC) <- md
 
     mirna <- acc[["miRNASeqGene"]]
-    mirna <-
-        mirna[apply(assay(mirna), 1, function(x)
-            sum(x >= 5)) >= 5,]
+    mirna <- mirna[rowSums(assay(mirna) >= 5) >= 5, ]
     experimentData(mirna)@abstract <-
         "Note: Rows not having at least 5 counts in at least 5 samples were removed."
     miniACC <- c(miniACC,
                  list(miRNASeqGene = mirna),
                  sampleMap = sampleMap(acc)[sampleMap(acc)$assay == "miRNASeqGene",])
 
-    save(miniACC, file = "miniACC.RData", compress = "bzip2")
+    save(miniACC, file = "data/miniACC.RData", compress = "bzip2")
 }
