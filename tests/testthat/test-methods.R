365c60565f4007c80d64d9988514c25db809fc33
LiNk-NY <mramos09@gmail.com>
2017-10-30 20:59:55 -0400

    fix shell class in test

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -1,48 +1,38 @@
 context("Methods tests for methods checks")
 
-library(SummarizedExperiment)
-setClass("MAEshell", slots = c(a = "RangedSummarizedExperiment"))
-setMethod("assay", "MAEshell", function(x) {assay(x@a)})
-setMethod("rownames", "MAEshell", function(x) {rownames(x@a)})
-setMethod("colnames", "MAEshell", function(x) {colnames(x@a)})
-setMethod("[", c("MAEshell", "ANY", "ANY"), function(x, i, j, ..., drop=TRUE) {
+test_that("the methods check out", {
+setClass("MAEshell", slots = c(assay = "matrix"))
+setMethod("assay", "MAEshell", function(x) {x@assay})
+setMethod("dimnames", "MAEshell", function(x) dimnames(x@assay))
+setMethod("[", c("MAEshell", "ANY", "ANY"), function(x, i, j, ...) {
     if (!missing(i) && !missing(j))
-        x@a[i, j, drop=drop]
+        x@assay[i, j]
     else if (missing(i))
-        x@a[, j, drop=drop]
+        x@assay[, j]
     else if (missing(j))
-        x@a[i, , drop=drop]
+        x@assay[i, ]
     else x
 })
-setMethod("dim", "MAEshell", function(x) {c(length(x@a),
-                                            length(assay(x@a)[1, ]))})
+setMethod("dim", "MAEshell", function(x) {
+    dim(x@assay)
+})
 
-nrows <- 5; ncols <- 4
-counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
-rowRanges <- GRanges(rep(c("chr1", "chr2"), c(2, nrows - 2)),
-                     IRanges(floor(runif(nrows, 1e5, 1e6)), width=100),
-                     strand=sample(c("+", "-"), nrows, TRUE),
-                     feature_id=sprintf("ID\\%03d", 1:nrows))
-names(rowRanges) <- letters[1:5]
-colData <- DataFrame(Treatment=rep(c("ChIP", "Input"), 2),
-                     row.names= c("mysnparray1", "mysnparray2",
-                                  "mysnparray3", "mysnparray4"))
-rse <- SummarizedExperiment(assays=SimpleList(counts=counts),
-                            rowRanges=rowRanges, colData=colData)
-
+nrows <- 5
+ncols <- 4
+sampNames <- c("mysnparray1", "mysnparray2", "mysnparray3", "mysnparray4")
+counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows,
+    dimnames = list(NULL, sampNames))
 masPheno <- data.frame(sex=c("M", "F", "M", "F"),
     age=38:41,
     row.names=c("Jack", "Jill", "Bob", "Barbara"))
 
-aShell <- new("MAEshell", a = rse)
+aShell <- new("MAEshell", assay = counts)
 newShell <- list(myShell = aShell)
 rangemap <-
     DataFrame(assay = factor("myShell"),
               primary = c("Jack", "Jill", "Bob", "Barbara"),
-              colname = c("mysnparray1", "mysnparray2", "mysnparray3",
-                          "mysnparray4"))
+              colname = sampNames)
 
-test_that("the methods check out", {
   expect_true(is(ExperimentList(newShell), "ExperimentList"))
   expect_true(is(MultiAssayExperiment(newShell, masPheno, rangemap),
                  "MultiAssayExperiment"))
