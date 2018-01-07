bf2ee264abbdb746f729905126b2e558dcea0abf
LiNk-NY <mramos09@gmail.com>
2018-01-06 21:04:02 -0500

    finally remove method imports from IRanges

--- R/MultiAssayExperiment-subset-methods.R
+++ R/MultiAssayExperiment-subset-methods.R
@@ -9,7 +9,7 @@
 #' @importFrom tidyr gather
 #' @importFrom GenomicRanges GRanges
 #' @importFrom IRanges IRanges findOverlaps subsetByOverlaps
-#' @importFrom IRanges splitAsList SplitDataFrameList endoapply mendoapply
+#' @importFrom IRanges splitAsList SplitDataFrameList
 #' @importFrom S4Vectors endoapply mendoapply
 #' @importFrom IRanges IntegerList CharacterList LogicalList
 #' @importFrom SummarizedExperiment findOverlaps assays
