6d5078cb26c9ce51c539066ab000138227bbea17
LiNk-NY <mramos09@gmail.com>
2018-06-27 15:10:53 -0400

    minor formatting in tests

--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -93,14 +93,27 @@ test_that("subsetByColumn works with lists", {
     expect_equal(length(experiments(myMultiAssayExperiment)[affySimple]),
                  length(affySimple))
 })
+test_that("subsetByColumn works with incomplete lists", {
+    fuLL <- colnames(myMultiAssayExperiment)
+    cLIST <- fuLL[c(1,3)]
+
+    expect_equal(length(myMultiAssayExperiment[, cLIST, , drop = FALSE]),
+        length(fuLL))
+    expect_equal(length(myMultiAssayExperiment[, cLIST, , drop = TRUE]),
+        length(cLIST))
+})
 
 test_that("subsetByColData works as intended", {
     trues <- sum(myMultiAssayExperiment$sex == "M")
-    expect_equal(nrow(colData(subsetByColData(myMultiAssayExperiment,
-                                          myMultiAssayExperiment$sex == "M"))),
-                 trues)
-    expect_equal(nrow(colData(
-        myMultiAssayExperiment[, myMultiAssayExperiment$sex == "M"])), trues)
+    expect_equal(
+        nrow(colData(subsetByColData(myMultiAssayExperiment,
+            myMultiAssayExperiment$sex == "M"))),
+        trues
+    )
+    expect_equal(
+        nrow(colData(myMultiAssayExperiment[, myMultiAssayExperiment$sex == "M"])),
+        trues
+    )
 })
 
 test_that("MultiAssayExperiment subsetting works with NULL rownames", {
@@ -108,13 +121,13 @@ test_that("MultiAssayExperiment subsetting works with NULL rownames", {
     ncols <- 6
     counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
     rowRanges <- GRanges(rep(c("chr1", "chr2"), c(50, 150)),
-                         IRanges(floor(runif(200, 1e5, 1e6)), width=100),
-                         strand=sample(c("+", "-"), 200, TRUE),
-                         feature_id=sprintf("ID%03d", 1:200))
+        IRanges(floor(runif(200, 1e5, 1e6)), width=100),
+        strand=sample(c("+", "-"), 200, TRUE),
+        feature_id=sprintf("ID%03d", 1:200))
     colData <- DataFrame(Treatment=rep(c("ChIP", "Input"), 3),
-                         row.names=LETTERS[1:6])
+        row.names=LETTERS[1:6])
     rse <- SummarizedExperiment(assays=SimpleList(counts=counts),
-                                rowRanges=rowRanges, colData=colData)
+        rowRanges=rowRanges, colData=colData)
     maeRSE <- MultiAssayExperiment(list(rag = rse))
     expect_true(is.character(rownames(maeRSE)[[1L]]))
     expect_true(length(rownames(maeRSE)[[1L]]) == 0L)
@@ -125,7 +138,8 @@ test_that("subsetting by row keeps the order of the subsettor", {
         dimnames = list(c("row1", "row2"), c("C1", "C2", "C3")))
     mdat2 <- mdat1[2:1, ]
     mae <- MultiAssayExperiment(list(A = mdat1, B = mdat2))
-    expectedOrder <- IRanges::CharacterList(A = c("row1", "row2"),
-                                            B = c("row1", "row2"))
+    expectedOrder <- IRanges::CharacterList(
+        A = c("row1", "row2"),
+        B = c("row1", "row2"))
     expect_identical(rownames(mae[c("row1", "row2")]), expectedOrder)
 })
