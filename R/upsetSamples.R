fd243da0d5b0569c8c62e0496fb21d58ea875ec1
LiNk-NY <mramos09@gmail.com>
2019-05-20 13:02:12 -0400

    update docs

--- R/upsetSamples.R
+++ R/upsetSamples.R
@@ -1,15 +1,19 @@
 #' Create a generalized Venn Diagram analog for sample membership in multiple
 #' assays, using the upset algorithm in \code{UpSetR}
 #'
-#' @param MultiAssayExperiment instance of
-#' \code{\link[MultiAssayExperiment]{MultiAssayExperiment-class}}
-#' @inheritParams UpSetR::upset
+#' @param MultiAssayExperiment A
+#' \code{\link[MultiAssayExperiment:MultiAssayExperiment-class]{MultiAssayExperiment}}
+#' instance
 #' @param nsets integer number of sets to analyze
 #' @param ... parameters passed to \code{\link[UpSetR]{upset}}
 #' @param nameFilter function, defaulting to force, to manipulate colnames of
 #' incidence matrix
 #' @param check.names logical(1) used when incidence matrix is coerced to
 #' data.frame for use in UpSetR::upset
+#' @param nintersects Number of intersections to plot. If set to NA, all
+#' intersections will be plotted.
+#' @param order.by How the intersections in the matrix should be ordered by.
+#' Options include frequency (entered as "freq"), degree, or both in any order.
 #'
 #' @note This function is intended to provide convenient visualization of assay
 #' availability configurations in MultiAssayExperiment instances. The
