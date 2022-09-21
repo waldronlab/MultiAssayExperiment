516e51d7f4444da6d9d0249900db7d8b2eb5c6d5
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-09-21 11:37:38 -0400

    add fill argument to listToMap for empty elements

--- R/listToMap.R
+++ R/listToMap.R
@@ -1,12 +1,17 @@
 #' @param listmap A named \code{list} object containing \code{DataFrame}s
-#' with "primary" and "colname" columns
+#'   with "primary" and "colname" columns
+#'
+#' @param fill logical(1) Whether to fill the map with an empty \code{DataFrame}
+#'   when empty elements are present in the input list
 #'
 #' @return A \linkS4class{DataFrame} class object of names
 #' @describeIn mapToList The inverse of the listToMap operation
 #' @export listToMap
-listToMap <- function(listmap) {
+listToMap <- function(listmap, fill = TRUE) {
     if (is.null(names(listmap)))
         stop("'listmap' must be a named list")
+    if (!BiocBaseUtils::isTRUEorFALSE(fill))
+        stop("'fill' must be a logical value")
 
     elementClass <- unique(vapply(listmap, class, character(1L)))
 
@@ -20,7 +25,7 @@ listToMap <- function(listmap) {
         listmap <- IRanges::DataFrameList(listmap)
 
     listmap <- lapply(listmap, function(lmap) {
-        if (isEmpty(lmap))
+        if (isEmpty(lmap) && fill)
             DataFrame(primary = NA_character_, colname = NA_character_)
         else
             lmap
