e508c2ee3ea236d417d53cdd5d06f093df0eb0c9
LiNk-NY <marcel.ramos@roswellpark.org>
2023-08-08 18:00:21 -0400

    wip update miniACC script

--- inst/scripts/miniACC.R
+++ inst/scripts/miniACC.R
@@ -7,28 +7,31 @@ library(MultiAssayExperiment)
 library(RaggedExperiment)
 library(Biobase)
 
-acc <- curatedTCGAData("ACC", "*", dry.run = FALSE)
+acc <- curatedTCGAData("ACC", "*", version = "2.1.1", dry.run = FALSE)
 proturl <- paste0("http://genomeportal.stanford.edu/",
     "pan-tcga/target_selection_send_data",
     "?filename=Allprotein.txt")
 protmap <- read.csv(proturl, as.is = TRUE)
 
+names(acc) <- gsub("ACC_(.*)-20160128", "\\1", names(acc))
+
 RPPAgenes <- Filter(nchar, protmap$Genes)
 RPPAgenes <- unlist(strsplit(RPPAgenes, ","))
 RPPAgenes <- unique(RPPAgenes)
 
-miniACC <-
-    acc[RPPAgenes, , c("RNASeq2GeneNorm", "gistict", "RPPAArray", "Mutations")]
-mut <- assay(miniACC[["Mutations"]], i = "Variant_Classification")
+miniACC <- acc[
+    RPPAgenes, ,
+    c("RNASeq2GeneNorm", "GISTIC_AllByGene", "RPPAArray", "Mutation")
+]
+mut <- assay(miniACC[["Mutation"]], i = "Variant_Classification")
 mut <- ifelse(is.na(mut) | mut == "Silent", 0, 1)
 
-miniACC[["Mutations"]] <- mut
+miniACC[["Mutation"]] <- mut
 colData(miniACC) <- colData(miniACC)[, c(1:17, 810:822)]
 
 rpparowData <-
     protmap[match(rownames(miniACC[["RPPAArray"]]), protmap$Genes),]
-rpparowData <- AnnotatedDataFrame(rpparowData)
-featureData(miniACC[["RPPAArray"]]) <- rpparowData
+rowData(miniACC[["RPPAArray"]]) <- rpparowData
 
 md <- list(
     title = "Comprehensive Pan-Genomic Characterization of Adrenocortical Carcinoma",
@@ -42,11 +45,18 @@ metadata(miniACC) <- md
 
 mirna <- acc[["miRNASeqGene"]]
 mirna <- mirna[rowSums(assay(mirna) >= 5) >= 5, ]
-experimentData(mirna)@abstract <-
-    "Note: Rows not having at least 5 counts in at least 5 samples were removed."
-miniACC <- c(miniACC,
-             list(miRNASeqGene = mirna),
-             sampleMap = sampleMap(acc)[sampleMap(acc)$assay == "miRNASeqGene",])
+metadata(mirna) <- list(
+    abstract =
+        paste(
+            "Note: Rows not having at least 5 counts in at",
+            "least 5 samples were removed."
+        )
+)
+miniACC <- c(
+    miniACC,
+    list(miRNASeqGene = mirna),
+    sampleMap = sampleMap(acc)[sampleMap(acc)$assay == "miRNASeqGene",]
+)
 
  miniACC[["RNASeq2GeneNorm"]] <-
      as(miniACC[["RNASeq2GeneNorm"]], "SummarizedExperiment")
